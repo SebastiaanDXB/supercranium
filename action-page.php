d7007f51bd0628c157f30713d10d22676c180b6a
Sebastiaan Jooste <sebastiaanjooste@gmail.com>
2020-11-16 17:21:39 +0200

    updated

--- action-page.php
+++ action-page.php
@@ -1,10 +1,77 @@
+<?php
+if (isset($_POST['Email'])) {
+
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "info@supercranium.com";
+    $email_subject = "New form submissions from Super Cranium landing page";
+
+    function problem($error)
+    {
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br><br>";
+        echo $error . "<br><br>";
+        echo "Please go back and fix these errors.<br><br>";
+        die();
+    }
+
+    // validation expected data exists
+    if (
+        !isset($_POST['Name']) ||
+        !isset($_POST['Email'])
+    ) 
+    {
+        problem('We are sorry, but there appears to be a problem with the form you submitted.');
+    }
+
+    $name = $_POST['Name']; // required
+    $email = $_POST['Email']; // required
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+    if (!preg_match($email_exp, $email)) {
+        $error_message .= 'The Email address you entered does not appear to be valid.<br>';
+    }
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+    if (!preg_match($string_exp, $name)) {
+        $error_message .= 'The Name you entered does not appear to be valid.<br>';
+    }
+
+    if (strlen($error_message) > 0) {
+        problem($error_message);
+    }
+
+    $email_message = "Form details below.\n\n";
+
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
+    }
+
+    $email_message .= "Name: " . clean_string($name) . "\n";
+    $email_message .= "Email: " . clean_string($email) . "\n";
+
+    // create email headers
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
+?>
+
+    <!-- include your success message below -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"/>
+    <link
+    rel="stylesheet"
+    href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"/>
     <link href="https://fonts.googleapis.com/css?family=Raleway:700&display=swap" rel="stylesheet">
     
     <title>Super Cranium</title>
@@ -49,23 +116,13 @@
     <meta name="twitter:creator" content="@thesupercranium">
 
 </head>
-<body>
-    <div class="main">
-    <h1 class="intro">Super Cranium - Gamewear is the new streetwear.</h1>
-    <div class="center"><img src="/images/Super-Cranium-logo.png" alt="super-cranium-logo"></div>
-    <h3 class="center space20">Be the first to know when we launch.</h3>
-    <div class="container center">
-        <form name="myform" method="POST" action="action-page.php">
-        
-            <input type="text" id="Name" name="Name" placeholder="Name" required>
 
-            <input type="email" id="Email" name="Email" placeholder="Email" required>
-        
-            <button class="button pulse" type="submit">
-                <i class="fa fa-paper-plane"></i> Sign Up 
-            </button>
-        </form>
-    </div>
+    <title>Super Cranium</title>
+</head>
+<body>
+    <div class="center"><img src="/images/Super-Cranium-logo.png" alt="logo"></div>
+    <h2 class="center space30">Thank you for contacting us. We will be in touch.</h2>
+
 
     <div class="space30"></div>
     <h3 class="center">Keep in touch with us. We <i class="fas fa-heart"></i> new friends.</h3>
@@ -75,6 +132,9 @@
         <a href="https://twitter.com/thesupercranium"><i class="fab fa-twitter padding10 social"></i></a>
         <a href="mailto:info@supercranium.com"><i class="far fa-envelope padding10 social"></i></a>
     </p>
-
 </body>
 </html>
+
+<?php
+}
+?>
